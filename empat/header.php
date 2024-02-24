de94c822f8306c885f5e240ea18e61802e3be0a6
Syau-Q <157093759+Syau-Q@users.noreply.github.com>
2024-02-24 13:39:34 +0700

    Add files via upload

---
+++ empat/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AN-NIDA BOARDING SCHOOL</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap/css/dataTables.bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap/css/styles.css" rel="stylesheet">
+		<style>
+      body {
+        padding-top: 60px;
+      }
+		</style>
+  </head>
+  <body>
+    
+	<!-- Awal script Navbar -->
+    <nav class="navbar navbar-primary navbar-inverse" id="scrollspy">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle Nav</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">AN-NIDA BOARDING SCHOOL</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+							<a href="index.php"> HOME </a>
+						</li>
+            <li>
+							<a href="profile.php"> PROFILE </a>
+						</li>
+		      	<li>
+							<a href="ppdb.php"> PPDB </a>
+						</li>
+		      	<li>
+							<a href="recruitment.php"> RECRUITMENT </a>
+			            </li>
+            <li>
+							<a href="contact.php"> CONTACT </a>
+						</li>
+            <li>
+              <a href="siswabaru.php"> SISWA BARU </a>
+            </li>
+		      	<li>
+							<a href="galery.php"> GALERY </a>
+						</li>
+            <li>
+							<a href="login.php"> LOGIN </a>
+						</li>
+
+          </ul>
+        </div>
+      </div>
+    </nav><!-- Akhir script Navbar -->
